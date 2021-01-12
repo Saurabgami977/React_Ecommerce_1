ac7b576ffc2ec2458e1b93e41edc729e57a405c7
Saurab Gami <saurabgami977@gmail.com>
2021-01-12 19:11:26 +0545

    broken link issue

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import Cart from './Components/Cart/Cart'
 function App() {
   return (
     // basename = "https://saurabgami977.github.io/React_Ecommerce_1"
-    <BrowserRouter >
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <div className="App">
-        <Route path={process.env.PUBLIC_URL + '/'} component={Navbar} />
+        <Route path='/' component={Navbar} />
         <Switch>
-          <Route path={process.env.PUBLIC_URL + '/'} exact component={Body} />
-          <Route path={process.env.PUBLIC_URL + "/product/:id"} exact component={ProductPage} />
-          <Route path={process.env.PUBLIC_URL + '/cart'} exact component={Cart} />
+          <Route path='/' exact component={Body} />
+          <Route path="/product/:id" exact component={ProductPage} />
+          <Route path='/cart' exact component={Cart} />
         </Switch>
       </div>
     </BrowserRouter>
